7d2e5187c162210859c46ba6328ccabf0be741ca
Nick Laid <Nicholas.Laid@go.mnstate.edu>
2023-03-28 14:39:54 -0500

    Added Create Account Page# Please enter the commit message for your changes. Lines starting

---
+++ Ticket Blaster/Pages/CreateAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ticket_Blaster.Pages
+{
+    public class CreateAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
