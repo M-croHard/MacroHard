7ad46bee3e25b830cc0ffe979b699be3e05e559e
Brady Bauck <ss7054hk@go.minnstate.edu>
2023-04-26 15:05:09 -0500

    Fixed namespace in page that casued website nt to launch

--- Ticket Blaster/Pages/Shared/App_code/Event.cs
+++ Ticket Blaster/Pages/Shared/App_code/Event.cs
@@ -4,26 +4,17 @@ namespace Event.Ticket_Blaster.Pages.Shared.App_code
 {
     public class Event
     {
-        //Attributes
-        private int EventID;
-        private string EventName;
-        private string EventHost;
-        private DateTime EventDate;
-        private string EventAddress;
-        private int MaxPeople;
+        // Properties
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public string EventHost { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventAddress { get; set; }
+        public int MaxPeople { get; set; }
+        public int TicketsSold { get; set; }
 
-        private int TicketsSold;
-
-        //Methods
-        protected int EventID { get; set; };
-        protected string EventName { get; set; };
-        protected string EventHost { get; set; };
-        protected string EventDate { get; set; };
-        protected string EventAddress { get; set; };
-        protected int MaxPeople { get; set; };
-        protected int TicketsSold { get; set; };
-
-        public Event(int eventID, string eventName, string eventHost, DateTime eventDate, string, eventAddress, int maxPeople)          //constructor
+        // Constructor
+        public Event(int eventID, string eventName, string eventHost, DateTime eventDate, string eventAddress, int maxPeople)
         {
             this.EventID = eventID;
             this.EventName = eventName;
@@ -33,8 +24,9 @@ namespace Event.Ticket_Blaster.Pages.Shared.App_code
             this.MaxPeople = maxPeople;
         }
 
-        public int ticketsLeft() {
+        public int ticketsLeft()
+        {
             return MaxPeople - TicketsSold;
-        };
+        }
     }
 }
