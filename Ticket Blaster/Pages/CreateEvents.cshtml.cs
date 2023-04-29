e6a6128dbe27b87657a2078718bfde525b9c8d02
Brady Bauck <ss7054hk@go.minnstate.edu>
2023-04-29 18:03:21 -0500

    Eli's Events Page reancarnated

---
+++ Ticket Blaster/Pages/CreateEvents.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ticket_Blaster.Pages
+{
+    public class CreateEvent : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
