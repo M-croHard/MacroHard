7bd12a2ab2fd87ef09a20224953bada41b7f72ea
Nicholas Laid <nicholas.laid@go.mnstate.edu>
2023-04-30 12:16:18 -0500

    Added Login,reg,logout but not working

---
+++ Ticket Blaster/Areas/Identity/Data/Ticket_BlasterUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ticket_Blaster.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Ticket_BlasterUser class
+public class Ticket_BlasterUser : IdentityUser
+{
+}
+
